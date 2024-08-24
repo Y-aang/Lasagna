506d71b8bbe59f1c2f56e7466fc36df900921331
Y-aang <2386139091@qq.com>
2024-08-24 10:41:26 -0400

    add: dataset protein demo

--- dataset.py
+++ dataset.py
@@ -1,6 +1,9 @@
 import dgl
-from dgl.data import RedditDataset, YelpDataset
+from dgl.data import RedditDataset, YelpDataset, TUDataset
 import torch.distributed as dist
+from dgl.partition import metis_partition
+import os
+from tqdm import tqdm
 
 class LasagnaDataset:
     
@@ -10,9 +13,11 @@ class LasagnaDataset:
             dataset = RedditDataset(raw_dir=datasetPath)
         elif datasetName == 'yelp':
             dataset = YelpDataset(raw_dir=datasetPath)
+        elif datasetName == 'proteins':
+            dataset = TUDataset(name='PROTEINS', raw_dir=datasetPath) 
             
         if self.rank == 0:
-            self.group_and_partition(dataset)
+            self.group_and_partition(dataset, savePath="./dataset")
             dist.barrier()
         else:
             print(dist.get_rank(), 'waiting...')
@@ -26,13 +31,28 @@ class LasagnaDataset:
     def __getitem__(self, index):
         pass
     
-    def group_and_partition(self, dataset):
+    def group_and_partition(self, dataset, savePath):
         print("process 0 processing data...")
-        # pairs = []
-        # for i in range(0, len(dataset), 2):
-        #     if i + 1 < len(graphs):
-        #         pairs.append([graphs[i], graphs[i + 1]])
-        #     else:
-        #         pairs.append([graphs[i]])
-        # return pairs
+
+        k = 4
+        os.makedirs(savePath, exist_ok=True)
+
+        for idx, data in enumerate(dataset):
+            print("processing No. ", idx)
+            graph = data[0]
+            if graph.is_homogeneous:
+                try:
+                    partitions = metis_partition(graph, k)
+                    
+                    graph_savePath = os.path.join(savePath, f'graph_{idx}')
+                    os.makedirs(graph_savePath, exist_ok=True)
+                    
+                    for i in range(k):
+                        part = partitions[i]
+                        dgl.save_graphs(os.path.join(graph_savePath, f'part_{i}.bin'), [part])
+                except Exception as e:
+                    print(f"Error partitioning graph {idx}: {e}")
+            else:
+                print(f"Graph {idx} is not homogeneous, skipping.")
+
     
--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import torch.multiprocessing as mp
 import torch.distributed as dist
 from torch.utils.data import DataLoader, DistributedSampler
 from dataset import LasagnaDataset
+import dgl
 
 def run(rank, device):
     print(f"Process {rank} running on device {device}")
     torch.cuda.set_device(device)
     
     datasetPath = "/data/yangshen/template/BNS-GCN/dataset"
-    dataset = LasagnaDataset("reddit", datasetPath)
+    dataset = LasagnaDataset("proteins", datasetPath)
     
 
 def init_process(rank, n_gpus):
@@ -34,3 +35,20 @@ if __name__ == "__main__":
     
     for p in processes:
         p.join()
+        
+        
+        
+    print("start loading ...")
+    loaded_partitions = []
+
+    part, _ = dgl.load_graphs(os.path.join("./dataset", 'graph_0', 'part_0.bin'))
+    loaded_partitions.append(part[0])
+
+    partition = loaded_partitions[0]
+    print("Partition 0:")
+    print(f"  Number of nodes: {partition.number_of_nodes()}")
+    print(f"  Number of edges: {partition.number_of_edges()}")
+    if partition.ndata:
+        print(f"  Node features: {list(partition.ndata.keys())}")
+    if partition.edata:
+        print(f"  Edge features: {list(partition.edata.keys())}")
