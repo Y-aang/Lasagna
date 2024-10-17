5e25394a35dd3df7ddc52106c02f3999977e09f5
Y-aang <yaangysshen@gmail.com>
2024-10-17 15:26:55 -0400

    start change construct graph

--- test/test.py
+++ test/test.py
@@ -100,21 +100,9 @@ def convert_map_to_local(recv_map, global_to_local_maps):
 
 
 # 第1步：加载TUDataset中的PROTEINS数据集
-dataset = TUDataset(name='PROTEINS')
-graph, label = dataset[0]
-print(f"图信息：{graph}")
-
-# 初始化节点特征
-# num_nodes = graph.num_nodes()
-# feat = torch.randn(num_nodes, 3)  # 假设每个节点有3维特征
-# graph.ndata['h'] = feat
-
-# edges = [(0, 1), (1, 2), (2, 3), (3, 0)]
-# u, v = zip(*edges)
-# graph = dgl.graph((u, v))
-# feat = torch.tensor([[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]], dtype=torch.float)
-# graph.ndata['h'] = feat
-# num_nodes = graph.num_nodes()
+# dataset = TUDataset(name='PROTEINS')
+# graph, label = dataset[0]
+# print(f"图信息：{graph}")
 
 num_nodes = 12
 # edges = [(i, (i + 1) % num_nodes) for i in range(num_nodes)]  # 形成圆环
@@ -139,6 +127,21 @@ tag = torch.tensor([
     [1], [2], [3], [4], [5], [6],
     [7], [8], [9], [10], [11], [12]
 ], dtype=torch.float)
+tag = torch.tensor([
+    [1, 2, 1],
+    [1, 3, 1],
+    [1, 4, 1],
+    [1, 5, 1],
+    [1, 6, 1],
+    [1, 7, 1],
+    [1, 8, 1],
+    [1, 9, 1],
+    [1, 10, 1],
+    [1, 11, 1],
+    [1, 12, 1],
+    [1, 13, 1]
+], dtype=torch.float)
+
 graph.ndata['h'] = feat
 graph.ndata['tag'] = tag
 print(graph.ndata)
